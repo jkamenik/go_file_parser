340b811b3e9e24b890f08d1423816b42d73e089d
John Kamenik <jkamenik@gmail.com>
2014-09-12 16:37:09 -0400

    Rewrite to chunk concurrently

--- src/main.go
+++ src/main.go
@@ -8,8 +8,13 @@ import (
   "encoding/csv"
 )
 
+type Chunk struct {
+  Header []string
+  Lines  [][]string
+}
+
 func main() {
-  fp, err := os.Open("test_data/test.csv.xz")
+  fp, err := os.Open(os.Args[1])
   if err != nil {
     log.Fatal(err)
   }
@@ -18,9 +23,66 @@ func main() {
   xz  := xzReader(fp)
   csv := csv.NewReader(xz)
 
-  header, err := csv.Read()
-  fmt.Println(header)
+  chunks, done := chunkFile(csv)
+  done = readChunks(chunks, done)
 
-  lines, err := csv.ReadAll()
-  fmt.Println(NewUnprocessedTrafficTable(header, lines))
+  select {
+    case <- done:
+      fmt.Println("Exiting")
+  }
+}
+
+func chunkFile(csv *csv.Reader) (chunks chan Chunk, done chan bool) {
+  chunkSize := 1000
+  chunks = make(chan Chunk, 10)
+  done   = make(chan bool)
+
+  go func(){
+    header, err := csv.Read()
+    if err != nil {
+      done <- true
+      return
+    }
+
+    lines := [][]string{}
+
+    for {
+      line, err := csv.Read()
+      if err != nil {
+        break
+      }
+      lines = append(lines, line)
+
+      if len(lines) >= chunkSize {
+        chunks <- Chunk{Header: header, Lines: lines}
+        lines = [][]string{}
+      }
+    }
+
+    if len(lines) >= 0 {
+      chunks <- Chunk{Header: header, Lines: lines}
+    }
+    done <- true
+  }()
+
+  return
+}
+
+func readChunks(chunks chan Chunk, chunkerDone chan bool) (done chan bool) {
+  done = make(chan bool)
+
+  go func(){
+    for {
+      select {
+        case chunk := <-chunks:
+          fmt.Println(chunk)
+        case _ = <-chunkerDone:
+          fmt.Println("Done")
+          done <- true
+          return
+      }
+    }
+  }()
+
+  return
 }
