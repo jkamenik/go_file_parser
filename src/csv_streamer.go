19bfd21dabec59f0bcab484bbe3164a2fe9df63e
John Kamenik <jkamenik@gmail.com>
2014-09-13 21:39:52 -0400

    Moving streaming to its own files and simplifying

---
+++ src/csv_streamer.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "encoding/csv"
+  "fmt"
+)
+
+type CsvLine struct {
+  Header []string
+  Line   []string
+}
+
+// streamCsv
+//  Streams a CSV Reader into a returned channel.  Each CSV row is streamed along with the header.
+//  "true" is sent to the `done` channel when the file is finished.
+//
+// Args
+//  csv    - The csv.Reader that will be read from.
+//  buffer - The "lines" buffer factor.  Send "0" for an unbuffered channel.
+func streamCsv(csv *csv.Reader, buffer int) (lines chan CsvLine) {
+  lines = make(chan CsvLine, buffer)
+
+  go func(){
+    // get Header
+    header, err := csv.Read()
+    if err != nil {
+      close(lines)
+      return
+    }
+
+    i := 0
+
+    for {
+      line, err := csv.Read()
+
+      if len(line) > 0 {
+        i++
+        lines <- CsvLine{Header: header, Line: line}
+      }
+
+      if err != nil {
+        fmt.Printf("Sent %d lines\n",i)
+        close(lines)
+        return
+      }
+    }
+  }()
+
+  return
+}
--- src/main.go
+++ src/main.go
@@ -10,14 +10,10 @@ import (
   "sync"
 )
 
-type CsvLine struct {
-  Header []string
-  Line   []string
-}
 
 type Line CsvLine
 
-const Buffer = 20
+const Buffer = 20000
 
 
 func main() {
@@ -30,92 +26,57 @@ func main() {
   xz  := xzReader(fp)
   csv := csv.NewReader(xz)
 
-  lines, done := streamFile(csv)
-  convertedLines, done := convertLine(lines,done)
-  done = printStream(convertedLines,done)
+  lines := streamCsv(csv, Buffer)
+  convertedLines := convertLine(lines)
+  completed := printStream(convertedLines)
 
   // halt until I am told we are done
-  <- done
-  fmt.Println("Done")
+  x := <- completed
+  fmt.Printf("Done %d lines\n", x)
   os.Exit(0)
 }
 
-func streamFile(csv *csv.Reader) (lines chan CsvLine, done chan bool) {
-  lines = make(chan CsvLine, Buffer)
-  done  = make(chan bool)
-
-  go func(){
-    // get Header
-    header, err := csv.Read()
-    if err != nil {
-      done <- true
-      return
-    }
-
-    for {
-      line, err := csv.Read()
-
-      if len(line) > 0 {
-        lines <- CsvLine{Header: header, Line: line}
-      }
-
-      if err != nil {
-        done <- true
-        return
-      }
-    }
-  }()
-
-  return
-}
-
-func convertLine(csvLines chan CsvLine,finished chan bool) (lines chan Line, done chan bool) {
+func convertLine(csvLines chan CsvLine) (lines chan Line) {
   lines = make(chan Line, Buffer)
-  done = make(chan bool)
 
-  process := func(processor int){
-    for {
-      select {
-        case line := <- csvLines:
-          // simulate a IO conversion
-          time.Sleep(1 * time.Millisecond)
-          fmt.Printf("%d: %s\n", processor, line)
-          lines <- Line(line)
-        case <- finished:
-          done <- true
-          return
-      }
+  process := func(processor int, group sync.WaitGroup){
+    defer group.Done()
+
+    for line := range csvLines {
+      // simulate a IO conversion
+      time.Sleep(1 * time.Millisecond)
+      // fmt.Printf("%d: %s\n", processor, line)
+      lines <- Line(line)
     }
+    fmt.Printf("Processor %d is done\n", processor)
   }
 
   go func(){
     var ws sync.WaitGroup
-    for i := 0; i < Buffer; i++ {
-      fmt.Println(i)
-      ws.Add(1)
-      go process(i)
+    ws.Add(1000)
+    for i := 0; i < 1000 ; i++ {
+      // ws.Add(1)
+      go process(i,ws)
     }
 
     ws.Wait()
-    done <- true
+    close(lines)
   }()
 
   return
 }
 
-func printStream(lines chan Line,finished chan bool) (done chan bool) {
-  done = make(chan bool)
+func printStream(lines chan Line) (done chan int) {
+  done = make(chan int)
 
   go func(){
-    for {
-      select {
-        case line := <- lines:
-          fmt.Println(line)
-        case <- finished:
-          done <- true
-          return
-      }
+    i := 0
+    for _ = range lines {
+      // fmt.Println(line)
+      i++
+      // fmt.Println(i)
     }
+    done <- i
   }()
 
   return
